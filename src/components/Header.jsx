c1ea544fb2e6b3cf0d7cfcf3f6f8f0d67ee66620
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-09-26 23:08:38 +0530

    jj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ import { FaMoon, FaSun, FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <VStack h={20} bg="rgba(0, 0, 0, 0.92)" >
+    <VStack w="100vw" h={20} bg="rgba(0, 0, 0, 0.92)" >
       <HStack w="100vw" pt='5'  alignItems="center" gap="2">
         
           <Heading  pl={46} fontSize={35} ml="1vw" color="#CBD5E0">
@@ -24,7 +24,8 @@ export default function Header() {
         <ButtonGroup>
           <IconButton
             size="sm"
-           
+            bg='rgb(32,33,36)'
+            color='gray.100'
             ml="1vw"
             icon={<FaMoon/>}
            isRound
@@ -37,6 +38,8 @@ export default function Header() {
             <IconButton
              size='sm'
               ml="1vw"
+              bg='rgb(32,33,36)'
+              color='gray.100'
               icon={<FaLinkedin/>}
               isRound='true'
               
@@ -45,7 +48,8 @@ export default function Header() {
           <a href="https://github.com/jackson-kanchanapally" target="_blank">
             <IconButton
               size="sm"
-              
+              bg='rgb(32,33,36)'
+              color='gray.100'
               ml="1vw"
               icon={<FaGithub/>}
               isRound
