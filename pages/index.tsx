10a15d596902e85e4fe17b36d56462060f622e7c
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-10-24 14:48:24 +0530

    rs

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import { ChakraProvider,Flex } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    
+    <ChakraProvider>
+<Flex pl='100'>
+  jack
+</Flex>
+    </ChakraProvider>
+  )
+}
+
+export default Home
