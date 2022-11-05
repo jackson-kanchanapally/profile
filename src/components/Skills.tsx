db0cc029ea8c885b174120a3293680286e55e4bf
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-11-05 18:02:57 +0530

    added updated App.txt

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,13 @@
 import {Box, List,
     ListItem,useMediaQuery,
-    UnorderedList,Heading} from '@chakra-ui/react';
+    UnorderedList,Heading,Text} from '@chakra-ui/react';
     
 export const Skills=()=>{
   const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
 return(
   <Box  bg="rgb(32,33,36)" pl={isNotSmallerScreen?150:10} pb='12'>
 <Box  pt='14'>
-        <Heading fontSize={isNotSmallerScreen?43:32} color="#CBD5E0" pb='5'>Programming Languages</Heading>
+        <Text fontSize={isNotSmallerScreen?43:32} color="#CBD5E0"   bgGradient="linear(to-l, #7928CA, #FF0080)" bgClip='text' pb='5' fontWeight={350}>Programming Languages</Text>
           <UnorderedList>
             <ListItem color='#CBD5E0' ml='5'>
               Java
@@ -20,7 +20,7 @@ return(
           
           </Box>
           <Box pt='10'>
-        <Heading fontSize={isNotSmallerScreen?43:32} color="#CBD5E0" pb='5'>Frontend</Heading>
+        <Heading fontSize={isNotSmallerScreen?43:32} color="#CBD5E0" pb='5'  bgGradient='linear(to-l, blue.700, red.300)' bgClip='text' fontWeight={400}>Frontend</Heading>
           <UnorderedList>
             <ListItem color='#CBD5E0' ml='5'>
               HTML
@@ -32,20 +32,20 @@ return(
           
           </Box>
           <Box  pt='10'>
-        <Heading fontSize={isNotSmallerScreen?43:32} color="#CBD5E0" pb='5'>Backend</Heading>
+        <Heading fontSize={isNotSmallerScreen?43:32} color="#CBD5E0" pb='5' fontWeight={400}>Backend</Heading>
           <UnorderedList>
             <ListItem color='#CBD5E0' ml='5'>Node Js</ListItem>
             <ListItem color='#CBD5E0' ml='5'>Express Js</ListItem>
           </UnorderedList>
           </Box>
           <Box pt='10'>
-        <Heading fontSize={isNotSmallerScreen?43:32} color="#CBD5E0" pb='5'>API</Heading>
+        <Heading fontSize={isNotSmallerScreen?43:32} color="#CBD5E0" pb='5' fontWeight={400}>API</Heading>
           <UnorderedList>
             <ListItem color='#CBD5E0' ml='5'>REST API</ListItem>
           </UnorderedList>
           </Box>
           <Box  pt='10'>
-        <Heading fontSize={isNotSmallerScreen?43:32} color="#CBD5E0" pb='5'>Database</Heading>
+        <Heading fontSize={isNotSmallerScreen?43:32} color="#CBD5E0" pb='5' fontWeight={400}>Database</Heading>
           <UnorderedList>
             <ListItem color='#CBD5E0' ml='5'>MySQL</ListItem>
             <ListItem color='#CBD5E0' ml='5'>MongoDB</ListItem>
